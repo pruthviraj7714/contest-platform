93220a84238cba41330b313ce39b888df868999f
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-09-21 23:44:33 +0530

    Added Zod Types

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -4,29 +4,36 @@ export const AuhtSchema = z.email({
   error: "Email Should be valid",
 });
 
-const ChallangeSchema = z.object({
+export const ChallangeSchema = z.object({
   index: z.number(),
   title: z.string(),
   description: z.string(),
   notionDocId: z.string(),
   startTime: z.date(),
   endTime: z.date(),
-  isActive: z.boolean(),
   maxPoints: z.number(),
 });
+
+export const EditChallangeSchema = z.object({
+  index: z.number(),
+  title: z.string(),
+  description: z.string(),
+  notionDocId: z.string(),
+  startTime: z.date(),
+  endTime: z.date(),
+  maxPoints: z.number(),
+});
+
 export const ContestSchema = z.object({
   title: z.string().min(5, {}),
   description: z.string().min(5, {}),
   startTime: z.date(),
   endTime: z.date(),
-  challenges: z.array(ChallangeSchema),
 });
 
 export const EditContestSchema = z.object({
-  id : z.string(),
   title: z.string().min(5, {}),
   description: z.string().min(5, {}),
   startTime: z.date(),
   endTime: z.date(),
-  challenges: z.array(ChallangeSchema),
 })
